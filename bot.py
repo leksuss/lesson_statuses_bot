759da549df84fa6db54831087c845bb5e54575eb
Andrey Alekseev <leksus@leksus.info>
2023-08-04 17:14:41 +0300

    send notification via bot

--- bot.py
+++ bot.py
@@ -1,17 +1,23 @@
 import requests
 import time
+import telegram
 
 from environs import Env
+
+
+def send_message(chat_id, text):
+
+    bot.send_message(chat_id=chat_id, text=text)
 
 
 if __name__ == '__main__':
     env = Env()
     env.read_env()
-    tg_token = env('TG_TOKEN')
-    devman_token = env('DEVMAN_TOKEN')
+
+    bot = telegram.Bot(token=env('TG_TOKEN'))
 
     headers = {
-        'Authorization': f'Token {devman_token}',
+        'Authorization': f"Token {env('DEVMAN_TOKEN')}",
     }
     timestamp = time.time()
     url = 'https://dvmn.org/api/long_polling/'
@@ -25,7 +31,6 @@ if __name__ == '__main__':
                 url,
                 headers=headers,
                 params=params,
-                timeout=5,
             )
         except requests.exceptions.ReadTimeout:
             continue
@@ -38,4 +43,5 @@ if __name__ == '__main__':
         response = response.json()
         if response['status'] == 'found':
             timestamp = response['last_attempt_timestamp']
-            print(response['new_attempts'])
+            send_message('Преподаватель проверил работу!')
+            # print(response['new_attempts'])
